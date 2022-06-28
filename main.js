c0b2f2c82a4608a21ef3cfe8d4fec2be2d2abd05
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2022-06-28 04:58:09 -0700

    create a rotating cube using three js

---
+++ main.js
@@ -0,0 +1,40 @@
+let camera, scene, renderer, cube;
+
+function init() {
+  scene = new THREE.Scene(); // determine what and where to render objects lights and camera.
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  ); // this type of camera gives a 3d view where objects closer look bigger and those far smaller
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const texture = new THREE.TextureLoader().load("wood.jpg");
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+}
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+init();
+animate();
